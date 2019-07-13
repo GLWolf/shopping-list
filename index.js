c58fe6fccbde5a14830e03d160caac12c038f721
Tony Bento <bentotony1010@yahoo.com>
2019-07-13 14:44:42 -0500

    Working code

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-function main(){
-function createShoppingList() {
-    $('#js-shopping-list-form').on('submit', function(e) {
+function main() {
+    function createShoppingList() {
+      $('#js-shopping-list-form').on('submit', function(e) {
         e.preventDefault()
         console.log('button click')
-    })
-}
-createShoppingList()
-}
-$(main)
+        const userItem = $('#shopping-list-entry').val()
+        console.log(userItem)
+        $('.shopping-list').append(`
+          <li>
+            <span class="shopping-item">${userItem}</span>
+            <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+              </button>
+              <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+              </button>
+            </div>
+          </li>
+          `)
+      })
+    }
+   
+    function checkItem() {
+      $('.shopping-list').on('click','.shopping-item-toggle', function(e) {
+        $(this).closest('li').children('.shopping-item').toggleClass('shopping-item__checked');
+        console.log('check button click')
+      })
+    }
+
+    function deleteItem(){
+        $('.shopping-list').on('click','.shopping-item-delete', function(e){
+            $(this).closest('li').remove()
+        })
+    }
+   
+   
+    createShoppingList()
+    checkItem()
+    deleteItem()
+   }
+   
+   $(main)
